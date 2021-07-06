cbe51aeb509565e969d79393a69bdf9f971dc6cd
hemanth2000 <33031429+hemanth2000@users.noreply.github.com>
2021-07-07 01:54:58 +0530

    added view engine, routes for homepage

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const router = require("./routes/");
 
+//1) Starting app
 const app = express();
 
+//2) MIDDLEWARES
+app.use("/", router);
+app.use(express.static("assests"));
+
+//Setting View engine and Templates location
+app.set("view engine", "ejs");
+app.set("views");
+
+// 3) Server
+// Setting the port for server
 const port = 8000;
 
 app.listen(port, (err) => {
